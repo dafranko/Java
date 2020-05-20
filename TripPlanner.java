7a2db1df7ec3e92d350ca58be015a316360af241
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-19 22:45:56 -0700

    Update TripPlanner.java

--- TripPlanner.java
+++ TripPlanner.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class TripPlanner{
 
 	public static void main(String[] args) {
-		Greetings();
+		/*Greetings();
 		System.out.println();
 		TravelBudget();
-		System.out.println();
+		System.out.println();*/
 		TimeDifference();
 
 	}
@@ -46,23 +46,40 @@ public class TripPlanner{
 		System.out.println("Your total budget in "+currency+" is "+moneyInTotal+" "+currency+", which per day is "+moneyPerDayDest+" "+currency);
 	}
 
+	//tells user the time difference
 	public static void TimeDifference(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("What is the time difference, in hours, between your home country and your destination? ");
-		int hourDiff0 = input.nextInt(); //difference in hours between user home and destination
+		int hourDiff0 = input.nextInt(); //difference in hours between user home and destination from user
 		int hourDiff1 = hourDiff0 % 24; //used for calculating time difference
 		int hourDiff3 = hourDiff1 + 24; //if input is negative
 		int hourDiff2 = hourDiff1 + 12; //if input is positive
-		if(hourDiff0 < 0){
-			//System.out.println(hourDiff1 - 12);
+		boolean flag = true;
+		System.out.println(hourDiff2);
+		if(hourDiff0 < 0){ //when input is negative
 			System.out.println("That means when it is midnight at home it will be "+hourDiff3+":00 in your travel destination and when it is noon at home it will be "+hourDiff2+":00");
-
+			flag = false;
 		}
-		else{
+
+		if(hourDiff0 == 12){ //when input is 12, needed for proper format
+			hourDiff2 = 0;
+			System.out.println("That means when it is midnight at home it will be "+hourDiff1+":00 in your travel destination and when it is noon at home it will be 0"+hourDiff2+":00");
+			flag = false;
+		}
+
+		if(hourDiff2 > 24 && flag){ //when the hour difference is greater than 24
+			hourDiff2 = hourDiff2 % 24; //gets the time value after 00:00
+			System.out.println("That means when it is midnight at home it will be "+hourDiff1+":00 in your travel destination and when it is noon at home it will be "+hourDiff2+":00");
+			flag = false;
+		}
+		if(flag){
 			System.out.println("That means when it is midnight at home it will be "+hourDiff1+":00 in your travel destination and when it is noon at home it will be "+hourDiff2+":00");
 		}
 
 	}
+
+
+
 
 
 }
