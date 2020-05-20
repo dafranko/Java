a631ac475e57688ed606b4d4d978cd71e9904434
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-19 23:08:30 -0700

    Update TripPlanner.java

--- TripPlanner.java
+++ TripPlanner.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class TripPlanner{
 
 	public static void main(String[] args) {
-		/*Greetings();
+		Greetings();
 		System.out.println();
 		TravelBudget();
-		System.out.println();*/
+		System.out.println();
 		TimeDifference();
 
 	}
@@ -41,6 +41,24 @@ public class TripPlanner{
 		double moneyInTotal = (double) money * conversion; //total money in destination currency
 		double moneyPerDayDest = (double) moneyInTotal / days; //money per day in destination currency
 
+		//to get two decimal places
+		moneyPerDayUS = moneyPerDayUS * 100;
+		moneyPerDayUS = (int) moneyPerDayUS;
+		moneyPerDayUS = (double) moneyPerDayUS;
+		moneyPerDayUS = moneyPerDayUS / 100.00;
+
+		//to get two decimal places
+		moneyInTotal = moneyInTotal * 100;
+		moneyInTotal = (int) moneyInTotal;
+		moneyInTotal = (double) moneyInTotal;
+		moneyInTotal = moneyInTotal / 100.00;
+
+		//to get two decimal places
+		moneyPerDayDest = moneyPerDayDest * 100;
+		moneyPerDayDest = (int) moneyPerDayDest;
+		moneyPerDayDest = (double) moneyPerDayDest;
+		moneyPerDayDest = moneyPerDayDest / 100.00;
+
 		System.out.println("If you are travelling for "+days+" days that is the same as "+hours+" hours or "+minutes+" minutes");
 		System.out.println("If you are going to spend $"+money+" USD that means per day you can spend up to $"+moneyPerDayUS+" USD");
 		System.out.println("Your total budget in "+currency+" is "+moneyInTotal+" "+currency+", which per day is "+moneyPerDayDest+" "+currency);
@@ -54,8 +72,8 @@ public class TripPlanner{
 		int hourDiff1 = hourDiff0 % 24; //used for calculating time difference
 		int hourDiff3 = hourDiff1 + 24; //if input is negative
 		int hourDiff2 = hourDiff1 + 12; //if input is positive
-		boolean flag = true;
-		System.out.println(hourDiff2);
+		boolean flag = true; //for if statements
+		
 		if(hourDiff0 < 0){ //when input is negative
 			System.out.println("That means when it is midnight at home it will be "+hourDiff3+":00 in your travel destination and when it is noon at home it will be "+hourDiff2+":00");
 			flag = false;
@@ -67,7 +85,7 @@ public class TripPlanner{
 			flag = false;
 		}
 
-		if(hourDiff2 > 24 && flag){ //when the hour difference is greater than 24
+		if(hourDiff2 > 24){ //when the hour difference is greater than 24
 			hourDiff2 = hourDiff2 % 24; //gets the time value after 00:00
 			System.out.println("That means when it is midnight at home it will be "+hourDiff1+":00 in your travel destination and when it is noon at home it will be "+hourDiff2+":00");
 			flag = false;
