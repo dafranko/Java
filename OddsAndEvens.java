a5d3d8c812ea6bd563a87b125970362b9a1a304e
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-21 15:31:13 -0700

    Update OddsAndEvens.java

--- OddsAndEvens.java
+++ OddsAndEvens.java
@@ -7,7 +7,7 @@ public class OddsAndEvens{
 
 	//method has the entire game
 	public static void Game(){
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); //nwe Scanner
 		System.out.println("Let's play a game called \"Odds and Evens\"");
 		System.out.print("What is your name? ");
 		String name = input.next(); //get user's name
@@ -29,6 +29,44 @@ public class OddsAndEvens{
 
 		System.out.println("---------------------------------------");
 
+		System.out.println();
 
+		System.out.print("How many \"fingers\" do you put out? ");
+		int userFingers = input.nextInt(); //# of fingers user will use
+
+		while(userFingers < 0 || userFingers > 5){ //if input is invalid
+			System.out.print("Invalid entry! Try again ");
+			userFingers = input.nextInt();
+		}
+
+		Random rand = new Random(); //new random
+		int computer = rand.nextInt(6); //get random number of fingers
+		System.out.println("The computer plays "+computer+" \"fingers\"");
+		System.out.println("----------------------------------------");
+
+		System.out.println();
+
+		int sum = userFingers + computer;
+		System.out.println(userFingers+" + "+computer+" = "+sum);
+
+		boolean decision = sum % 2 == 0;
+
+		if(decision){ //if even
+			if(choice.equals("E")){
+				System.out.println(sum+" is ...even!");
+				System.out.println("That means "+name+" wins! :)");
+			}else{
+				System.out.println(sum+" is ...even!");
+				System.out.println("That means the computer wins! :(");
+			}
+		}else{
+			if(choice.equals("O")){
+				System.out.println(sum+" is ...odd!");
+				System.out.println("That means "+name+" wins! :)");	
+			}else{
+				System.out.println(sum+" is ...odd!");
+				System.out.println("That means the computer wins! :(");
+			}
+		}
 	}
 }
