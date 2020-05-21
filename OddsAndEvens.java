7a89d1487ee26d2e86c7bc9d257da85adf6b45dd
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-20 21:25:59 -0700

    Update OddsAndEvens.java

--- OddsAndEvens.java
+++ OddsAndEvens.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class OddsAndEvens{
 	public static void main(String[] args){
-		
+		Pick();
+	}
+
+	//greet user and user's pick of even or odd
+	public static void Pick(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Lets play a game called \"Odds and Evens\"");
+		System.out.print("What is your name? ");
+		String name = input.next(); //get user's name
+		System.out.print("Hi "+name+", which fo you choose? (O)dds or (E)vens ");
+		String choice = input.next(); //get user choice
+
+		while(!(choice.equals("O")) && !(choice.equals("E"))){ //if input is invalid
+			System.out.print("Invalid entry! Try again ");
+			choice = input.next();
+		}
+
+		if(choice.equals("O")){
+			System.out.println(name+" has picked odds! The computer will be evens.");
+		}
+
+		if(choice.equals("E")){
+			System.out.println(name+" has picked evens! The computer will be odds.");
+		}
 	}
 }
