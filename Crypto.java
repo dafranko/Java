7be25def1290993e33a9fe05b382b93fd7290dd2
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-21 16:31:31 -0700

    Create Crypto.java

---
+++ Crypto.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Crypto{
+	public static void main(String[] args){
+
+	}
+
+	public static String normalizeText(String text){
+		Scanner input = new Scanner(System.in);
+		for(int i = 0; i <= text.length(); i++){
+
+		}
+		return text;
+	}
+}
