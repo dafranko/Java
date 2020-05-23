ec438e96421af916ec5640efe81869257d55aef8
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-22 19:43:20 -0700

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Crypto{
 	public static void main(String[] args){
 		String crypt = normalizeText("Hippies!");
-		String ceasar = ceasarify(crypt , 1);
+		String ceasar = ceasarify(crypt , 2);
+		String grouped = groupify(ceasar, 2);
 		System.out.println(crypt);
 		System.out.println(ceasar);
 	}
 
+	//nomralize the text
 	public static String normalizeText(String text){
 		text = text.replace(" ", "");
 		text = text.toUpperCase();
@@ -42,37 +44,45 @@ public class Crypto{
 		return text; 
 	}
 
+	//shift the text by the key value
 	public static String ceasarify(String text, int key){
 		String ceasar = ""; //encrypted text
 		String alpha = shiftAlphabet(key); //alphabet shifted by key
 
 		for(int j = 0; j < text.length(); j++){
-			int index = j + 1;
-			String str = text.substring(j,index);
-			//get index value of str from alpha
-			//add index value to get the ceasar equivalent
-			System.out.println(str);
+			int index = j + 1; //to get single char in string
+			String str = text.substring(j,index); //the char in string
+			int strVal = alpha.indexOf(str); //get index value of str from alpha
+			String temp = ""; //to temporarly store char of string
+			temp = alpha.substring(strVal+key,strVal+key+1); //add index value to get the ceasar char equivalent
+			ceasar = ceasar + temp; //add ceasar equivalent to final return string 
 		}
 		return ceasar;
 	} 
 
+	//alphabet that is shifted by shift value, provided by edX course
 	public static String shiftAlphabet(int shift) {
-    int start = 0;
-    if (shift < 0) {
-        start = (int) 'Z' + shift + 1;
-    } else {
-        start = 'A' + shift;
-    }
-    String result = "";
-    char currChar = (char) start;
-    for(; currChar <= 'Z'; ++currChar) {
-        result = result + currChar;
-    }
-    if(result.length() < 26) {
-        for(currChar = 'A'; result.length() < 26; ++currChar) {
-            result = result + currChar;
-        }
-    }
-    return result;
-}
+    	int start = 0;
+    	if (shift < 0) {
+        	start = (int) 'Z' + shift + 1;
+   		} else {
+        	start = 'A' + shift;
+    	}
+    	String result = "";
+    	char currChar = (char) start;
+    	for(; currChar <= 'Z'; ++currChar) {
+        	result = result + currChar;
+    	}
+    	if(result.length() < 26) {
+        	for(currChar = 'A'; result.length() < 26; ++currChar) {
+            	result = result + currChar;
+        	}
+    	}
+    	return result;
+	}
+
+	//group the text letters by the group value 
+	public static String groupify(String text, int group){
+
+	}
 }
