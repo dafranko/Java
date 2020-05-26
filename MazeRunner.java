ca27ba7ca03bb714ea50e8ed70e10072c3eea466
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-25 20:33:53 -0700

    Update MazeRunner.java

--- MazeRunner.java
+++ MazeRunner.java
@@ -4,8 +4,8 @@ public class MazeRunner{
 	public static Maze myMap=new Maze(); //call the maze class provided by edx course
 
 	public static void main(String[] args){
-		intro();
-		String game = userMove();
+		intro(); //intro to maze game
+		userMove(); //maze game
 	}
 
 	public static void intro(){
@@ -14,128 +14,145 @@ public class MazeRunner{
 		myMap.printMap();
 	}
 
-	public static String userMove(){
+	public static void userMove(){
 		Scanner input = new Scanner(System.in);
-		String dir = "";
-		String pit = "";
-		int moves = 0;
-		//might need a big while loop here
-		while(!(myMap.didIWin()) && !(moves == 99)){
-			System.out.print("Where will you like to move? (R, L, U, D) ");
-			dir = input.next();
-			dir = dir.toUpperCase();
-			System.out.println(dir);
+		String dir = ""; //direction from user
+		String pit = ""; //yes or no for user to jump pit
+		int moves = 0; //the total number of moves
 
+		//loop dtermines if game is over
+		while(!(myMap.didIWin()) && !(moves == 99)){ //did user win or did he pass the number of moves
+			System.out.print("Where will you like to move? (R, L, U, D) ");
+			dir = input.next(); //direction from user
+			dir = dir.toUpperCase(); //uppercase the input
+
+			//input validation
 			while(!(dir.equals("R") || dir.equals("L") || dir.equals("U") || dir.equals("D"))){
 				System.out.print("Invalid entry ");
 				dir = input.next();
 				dir = dir.toUpperCase();
 			}
 
+			//switch case for user input
 			switch(dir){
-				case ("R"):	
-					if(myMap.canIMoveRight()){
-						myMap.moveRight();
-						myMap.printMap();
-					}else if(myMap.isThereAPit("R")){
-						System.out.println("Watch out! There is a pit. Jump? ");
+
+				case ("R"):	//user chose right
+					if(myMap.canIMoveRight()){ //is space available
+						myMap.moveRight(); //move right
+						myMap.printMap(); //update map
+					}else if(myMap.isThereAPit("R")){ //is there a pit?
+						System.out.print("Watch out! There is a pit. Jump? ");
 						pit = input.next();
-						if(pit.equalsIgnoreCase("Y")){
+						if(pit.equalsIgnoreCase("Y")){ //move right if user wants to, update map
 							myMap.jumpOverPit("R");
 							myMap.printMap();	
 						}
 						else{
-							System.out.println("Not jumping ");
+							System.out.println("Not jumping "); //user doesnt want to jump
+							moves = moves - 1; //invalid move
 						}
 					}
 					else{
-						System.out.println("Error! Hit a wall");
+						System.out.println("Error! Hit a wall"); //a wall was enounctered
+						moves = moves - 1; //invalid move
 					}
 					break;
 
-				case ("L"):	
-					if(myMap.canIMoveLeft()){
-						myMap.moveLeft();
-						myMap.printMap();
-					}
-					else if(myMap.isThereAPit("L")){
-						System.out.println("Watch out! There is a pit. Jump? ");
+				case ("L"):	//user chose left
+					if(myMap.canIMoveLeft()){ //is space available
+						myMap.moveLeft(); //move left
+						myMap.printMap(); //update map
+					}else if(myMap.isThereAPit("L")){ //is there a pit?
+						System.out.print("Watch out! There is a pit. Jump? ");
 						pit = input.next();
-						if(pit.equalsIgnoreCase("Y")){
+						if(pit.equalsIgnoreCase("Y")){ //move left if user wants to, update map
 							myMap.jumpOverPit("L");
 							myMap.printMap();
 						}
 						else{
-							System.out.println("Not jumping ");
+							System.out.println("Not jumping "); //user doesnt want to jump
+							moves = moves - 1; //invalid move
 						}
 					}
 					else{
 						 System.out.println("Error! Hit a wall");
+						 moves = moves - 1; //invalid move
 					}
 					break;
 
-				case ("U"):	
-					if(myMap.canIMoveUp()){
-						myMap.moveUp();
-						myMap.printMap();
-					}else if(myMap.isThereAPit("U")){
-						System.out.println("Watch out! There is a pit. Jump? ");
+				case ("U"):	//user chose up
+					if(myMap.canIMoveUp()){ //is space available
+						myMap.moveUp(); //move up
+						myMap.printMap(); //update map
+					}else if(myMap.isThereAPit("U")){ //is there a pit?
+						System.out.print("Watch out! There is a pit. Jump? ");
 						pit = input.next();
-						if(pit.equalsIgnoreCase("Y")){
+						if(pit.equalsIgnoreCase("Y")){ //move up if user wants, update map
 							myMap.jumpOverPit("U");
 							myMap.printMap();
 						}
 						else{
-							System.out.println("Not jumping ");
+							System.out.println("Not jumping "); //user doesnt want to jump
+							moves = moves - 1; //invalid move
 						}
-											}
+					}
 					else{
 						 System.out.println("Error! Hit a wall");
+						 moves = moves - 1; //invalid move
 					}
 					break;
 
-				case ("D"):	
-					if(myMap.canIMoveDown()){
-						myMap.moveDown();
-						myMap.printMap();
+				case ("D"):	//user chose down
+					if(myMap.canIMoveDown()){ //is space available
+						myMap.moveDown(); //move down
+						myMap.printMap(); //update map
 					}
-					else if(myMap.isThereAPit("D")){
-						System.out.println("Watch out! There is a pit. Jump? ");
+					else if(myMap.isThereAPit("D")){ //is there a pit?
+						System.out.print("Watch out! There is a pit. Jump? ");
 						pit = input.next();
-						if(pit.equalsIgnoreCase("Y")){
+						if(pit.equalsIgnoreCase("Y")){ //move down if user wants, update map
 							myMap.jumpOverPit("D");
 							myMap.printMap();
 						}
 						else{
-							System.out.println("Not jumping ");
+							System.out.println("Not jumping "); //user doesnt want to jump
+							moves = moves - 1; //invalid move
 						}
 					}
 					else{
 						 System.out.println("Error! Hit a wall");
+						 moves = moves - 1; //invalid move
 					}
 					break;
 			}
-			moves = moves + 1;
-			movesMessage(moves);
-		}
-		
-		if(moves == 99){
 
-		}else{
-			System.out.println("You win");
+			moves = moves + 1; //update the number of moves, stays the same if move is invalid
+
+			if(moves < 0){ //for the beginning, if moves becasue less than 0
+				moves = 0;
+			}
+
+			movesMessage(moves);
+			System.out.println("***************************************************************");
 		}
-		return dir;	
+
+		if(moves == 99){
+			//do nothing, function moveMessage displays loss of game
+		}else{
+			System.out.println("You win"); //if user won under 100 moves
+		}	
 	}
 
+	//to warn user the number of moves available
 	public static void movesMessage(int numberMoves){
 		if(numberMoves == 50){
-			System.out.println("Warning: You have made 50 moves, you have 50 remaining before the maze exit closes");
+			System.out.println("***Warning: You have made 50 moves, you have 50 remaining before the maze exit closes***");
 		}
 		if(numberMoves == 75){
-			System.out.println("Alert! You have made 75 moves, you only have 25 moves left to escape");
+			System.out.println("***Alert! You have made 75 moves, you only have 25 moves left to escape***");
 		}
 		if(numberMoves == 90){
-			System.out.println("Danger! You have made 90 moves, you only have 10 moves left to escape");
+			System.out.println("***Danger! You have made 90 moves, you only have 10 moves left to escape***");
 		}
 		if(numberMoves == 99){
 			System.out.println("Oh no! You took to long to escape, and now the maze exit is closed FOREVER >:[");
