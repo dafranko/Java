e3d84292fdad4a677a1bcdfca762df59873ee36b
dafranko <49261488+dafranko@users.noreply.github.com>
2020-05-25 20:05:22 -0700

    Update MazeRunner.java

--- MazeRunner.java
+++ MazeRunner.java
@@ -6,7 +6,6 @@ public class MazeRunner{
 	public static void main(String[] args){
 		intro();
 		String game = userMove();
-		System.out.println("You win");	
 	}
 
 	public static void intro(){
@@ -19,8 +18,9 @@ public class MazeRunner{
 		Scanner input = new Scanner(System.in);
 		String dir = "";
 		String pit = "";
+		int moves = 0;
 		//might need a big while loop here
-		while(!(myMap.didIWin())){
+		while(!(myMap.didIWin()) && !(moves == 99)){
 			System.out.print("Where will you like to move? (R, L, U, D) ");
 			dir = input.next();
 			dir = dir.toUpperCase();
@@ -115,7 +115,30 @@ public class MazeRunner{
 					}
 					break;
 			}
+			moves = moves + 1;
+			movesMessage(moves);
 		}
-		return dir;
+		
+		if(moves == 99){
+
+		}else{
+			System.out.println("You win");
+		}
+		return dir;	
+	}
+
+	public static void movesMessage(int numberMoves){
+		if(numberMoves == 50){
+			System.out.println("Warning: You have made 50 moves, you have 50 remaining before the maze exit closes");
+		}
+		if(numberMoves == 75){
+			System.out.println("Alert! You have made 75 moves, you only have 25 moves left to escape");
+		}
+		if(numberMoves == 90){
+			System.out.println("Danger! You have made 90 moves, you only have 10 moves left to escape");
+		}
+		if(numberMoves == 99){
+			System.out.println("Oh no! You took to long to escape, and now the maze exit is closed FOREVER >:[");
+		}
 	}
 }
