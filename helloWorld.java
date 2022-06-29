3953642c2e7298d39adbcd0bdbeb40d079055fce
dafranko <49261488+dafranko@users.noreply.github.com>
2022-06-28 21:54:47 -0700

    Branch Edit

---
+++ helloWorld.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class helloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello");
+
+		int firstNum = (10+12) + (2*4);
+
+		int secondNum = 12 * 5;
+
+		int thirdNum = 13;
+
+		System.out.println("Edit-1");
+
+		System.out.println((firstNum+secondNum*thirdNum)-1000);
+	}
+}
